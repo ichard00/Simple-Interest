7b5fdd6e6f21ff5beaa8ddbc6f02474b87432e3e
ichard00 <richardolimamogaji@gmail.com>
2025-02-12 09:29:25 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function calculate(){
+    var p = 0;
+    var n = 0;
+    var r = 0;
+    var si = 0;
+    p = document.getElementById("Principal");
+    n = document.getElementById("Years");
+    r = document.getElementById("rate");
+    si =  (p*n*r)/100;
+    document.write("res").in
+}
