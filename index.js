dbebaa0c5b9d2647b709254b2860233d33dfc4f2
ichard00 <richardolimamogaji@gmail.com>
2025-02-12 09:39:52 +0100

    my first chages

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function calculate(){
     var n = 0;
     var r = 0;
     var si = 0;
-    p = document.getElementById("Principal");
-    n = document.getElementById("Years");
-    r = document.getElementById("rate");
+    p = document.getElementById("Principal") value;
+    n = document.getElementById("Years")value;
+    r = document.getElementById("rate")value;
     si =  (p*n*r)/100;
-    document.write("res").in
+    document.write("res").innerHTML = si;
 }
